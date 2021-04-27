61234722bd306140370f13cd61177d8932cfbe71
Gioele <gioele.armiento@gmail.com>
2021-04-27 17:13:42 +0200

    Wroking on ranks section

--- dist/js/app.js
+++ dist/js/app.js
@@ -49,6 +49,74 @@ function initVue() {
         "title": "Amazingly fresh fruit and herb drinks for summer",
         "descr": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, corporis. Cum quisquam, temporibus delectus quas veritatis eveniet, recusandae ipsa perspiciatis ab eaque dolore eum voluptatibus id iusto maxime voluptate minus!"
       }],
+      "popularPosts": [{
+        "id": "popular0",
+        "icon": "img/blog-55.jpg",
+        "title": "Simple Ways to Have a Pretty Face",
+        "date": "January 12, 2019"
+      }, {
+        "id": "popular1",
+        "icon": "img/blog-56.jpg",
+        "title": "Ranking the greates players in basketball",
+        "date": "January 12, 2019"
+      }, {
+        "id": "popular2",
+        "icon": "img/blog-57.jpg",
+        "title": "4 ways to Look Cool in Glasses",
+        "date": "January 12, 2019"
+      }, {
+        "id": "popular3",
+        "icon": "img/blog-58.jpg",
+        "title": "Top Camper Trailer Towing Tips",
+        "date": "January 12, 2019"
+      }, {
+        "id": "popular4",
+        "icon": "img/blog-59.jpg",
+        "title": "5 Lovely Walks in New York",
+        "date": "January 12, 2019"
+      }],
+      "recentPosts": [{
+        "id": "recent0",
+        "icon": "img/blog-65.jpg",
+        "title": "Main Reasons To Stop Texting And Driving",
+        "date": "January 12, 2019"
+      }, {
+        "id": "recent1",
+        "icon": "img/blog-66 (1).jpg",
+        "title": "Tips To Help You Quickly Prepare your lunch",
+        "date": "January 12, 2019"
+      }, {
+        "id": "recent2",
+        "icon": "img/blog-67.jpg",
+        "title": "Why should I buy a smartwatch?",
+        "date": "January 12, 2019"
+      }, {
+        "id": "recent3",
+        "icon": "img/blog-68.jpg",
+        "title": "The best augmented reality smartglasses",
+        "date": "January 12, 2019"
+      }, {
+        "id": "recent4",
+        "icon": "img/blog-69.jpg",
+        "title": "12 Healties Foods to eat for breakfast",
+        "date": "January 12, 2019"
+      }],
+      "slider": [{
+        "id": "slider0",
+        "img": "img/blog-56 (1).jpg",
+        "label": "technology",
+        "title": "Main reasons to stop texting and driving"
+      }, {
+        "id": "slider1",
+        "img": "img/blog-66-jpg",
+        "label": "recipes",
+        "title": "Tips to help you quickly prepare your lunch"
+      }, {
+        "id": "slider2",
+        "img": "img/blog-67.jpg",
+        "label": "gadgets",
+        "title": "Why should I buy a smartwatch?"
+      }],
       "showDescr": false,
       "activePreview": ""
     },
