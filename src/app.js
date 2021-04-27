a21a2a77ec0302dbb02636d0aec23b9e55196510
Gioele <gioele.armiento@gmail.com>
2021-04-27 15:44:06 +0200

    Thubnalis section added

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ function initVue() {
             "previews" : [
                 {   
                     "id" : "post0",
-                    "img" : "../img/blog-46.jpg",
+                    "img" : "img/blog-46.jpg",
                     "label" : "Photography",
                     "title" : "How to Better Concert Pictures in 30 Seconds",
                     "descr" : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, corporis. Cum quisquam, temporibus delectus quas veritatis eveniet, recusandae ipsa perspiciatis ab eaque dolore eum voluptatibus id iusto maxime voluptate minus!"
@@ -28,7 +28,7 @@ function initVue() {
 
                 {   
                     "id" : "post1",
-                    "img" : "../img/blog-47.jpg",
+                    "img" : "img/blog-47.jpg",
                     "label" : "Gadgets",
                     "title" : "Gadgets that make your smartphone even smarter",
                     "descr" : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, corporis. Cum quisquam, temporibus delectus quas veritatis eveniet, recusandae ipsa perspiciatis ab eaque dolore eum voluptatibus id iusto maxime voluptate minus!"
@@ -36,7 +36,7 @@ function initVue() {
 
                 {   
                     "id" : "post2",
-                    "img" : "../img/blog-48.jpg",
+                    "img" : "img/blog-48.jpg",
                     "label" : "Travel",
                     "title" : "20 Top-rated tourist attractions in manhattan",
                     "descr" : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, corporis. Cum quisquam, temporibus delectus quas veritatis eveniet, recusandae ipsa perspiciatis ab eaque dolore eum voluptatibus id iusto maxime voluptate minus!"
@@ -44,7 +44,7 @@ function initVue() {
 
                 {   
                     "id" : "post3",
-                    "img" : "../img/blog-49.jpg",
+                    "img" : "img/blog-49.jpg",
                     "label" : "Lifestyle",
                     "title" : "The best way to ride a motorcycle",
                     "descr" : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, corporis. Cum quisquam, temporibus delectus quas veritatis eveniet, recusandae ipsa perspiciatis ab eaque dolore eum voluptatibus id iusto maxime voluptate minus!"
@@ -52,7 +52,7 @@ function initVue() {
 
                 {   
                     "id" : "post4",
-                    "img" : "../img/blog-50.jpg",
+                    "img" : "img/blog-50.jpg",
                     "label" : "Travel",
                     "title" : "5 fun things to do at the beach",
                     "descr" : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, corporis. Cum quisquam, temporibus delectus quas veritatis eveniet, recusandae ipsa perspiciatis ab eaque dolore eum voluptatibus id iusto maxime voluptate minus!"
@@ -60,14 +60,24 @@ function initVue() {
 
                 {   
                     "id" : "post5",
-                    "img" : "../img/blog-51.jpg",
+                    "img" : "img/blog-51.jpg",
                     "label" : "Recipes",
                     "title" : "Amazingly fresh fruit and herb drinks for summer",
                     "descr" : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, corporis. Cum quisquam, temporibus delectus quas veritatis eveniet, recusandae ipsa perspiciatis ab eaque dolore eum voluptatibus id iusto maxime voluptate minus!"
                 },
+            ],
 
+            "showDescr" : false,
+            "activePreview" : ""
+        },
 
-            ]
+        methods: {
+
+            showDescription: function(id) {
+
+                this.activePreview = id;
+                this.showDescr = true;
+            }
         }
     })
 }
